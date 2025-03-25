b5fb27e57ac681abc0dd9328fb1d3e35b3390494
Miaritsoa2424 <tsoamiari24@gmail.com>
2025-03-25 15:32:49 +0300

    Ajout des link css

--- app/views/template.php
+++ app/views/template.php
@@ -8,7 +8,8 @@
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="<?= Flight::get('flight.base_url') ?>/public/assets/css/template.css">
-
+    <link rel="stylesheet" href="<?= Flight::get('flight.base_url') ?>/public/assets/css/validation.css">
+    <link rel="stylesheet" href="<?= Flight::get('flight.base_url') ?>/public/assets/css/budget.css">
     <style>
 
     </style>
@@ -17,9 +18,9 @@
 <body>
     <div class="navbar">
         <a href="#"><i class="fas fa-home"></i>Accueil</a>
-        <a href="#"><i class="fas fa-wallet"></i>Budget</a>
+        <a href="budget"><i class="fas fa-wallet"></i>Budget</a>
         <a href="#"><i class="fas fa-building"></i>Département</a>
-        <a href="#"><i class="fas fa-check-circle"></i>Validation</a>
+        <a href="validation"><i class="fas fa-check-circle"></i>Validation</a>
         <a href="#"><i class="fas fa-info-circle"></i>À propos</a>
     </div>
 
